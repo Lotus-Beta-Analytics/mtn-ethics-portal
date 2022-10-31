9d436da72ea55937263652a2d1dbb9f430bb6038
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2022-10-31 09:58:52 +0100

    feat: quiz management

---
+++ src/webparts/ethicsPortal/components/modules/admin/pages/quiz/modals/DeleteQuizModal.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+type Props = {
+  QuizTitle: string;
+  QuizId: number;
+};
+
+export const DeleteQuizModal: React.FC<Props> = ({ QuizTitle, QuizId }) => {
+  return <div>DeleteQuizModal</div>;
+};
