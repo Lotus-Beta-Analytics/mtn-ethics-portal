84cca0d0e05b5e59770f4c4f62520cc9c55e702e
FonsusAli <fonsusali@yahoo.com>
2022-11-03 18:14:38 +0100

    Update Training

--- src/webparts/ethicsPortal/components/modules/employee/pages/training/training-ethics-business/BusinessEthics.tsx
+++ src/webparts/ethicsPortal/components/modules/employee/pages/training/training-ethics-business/BusinessEthics.tsx
@@ -1,21 +1,22 @@
+import { Box } from "@material-ui/core";
 import * as React from "react";
+import { TrainingCategoryEnum } from "../../../../admin/pages/training/enums/TrainingCategoryEnum";
 import { EmployeeWrapper } from "../../../../shared/components/app-wrapper/employee/EmployeeWrapper";
 import { PageWrapper } from "../../../../shared/components/app-wrapper/employee/PageWrapper";
 import { PageHeaderWithImage } from "../../../../shared/components/PageHeaderWithImage";
+import { ResourcesDisplayComponent } from "../../../components/resources/ResourcesDisplayComponent";
 import "./styles.css";
 
 export const BusinessEthics = () => {
   return (
-    <EmployeeWrapper>
-      <PageWrapper>
-        <PageHeaderWithImage
-          bg="https://mtncloud.sharepoint.com/sites/MTNAppDevelopment/ethicsportal/assets/mtn-training.png"
-          text="Training"
+    <PageWrapper>
+      <Box>
+        <ResourcesDisplayComponent
+          backgroundImage="https://mtncloud.sharepoint.com/sites/MTNAppDevelopment/ethicsportal/assets/mtn-training.png"
+          pageTitle="Business Ethics Everyone's Responsibilities Videos"
+          filter={TrainingCategoryEnum.Business_Ethics}
         />
-        <div className="titleH3">
-          <h3>Business Ethics Everyone's Responsibilities Videos</h3>
-        </div>
-      </PageWrapper>
-    </EmployeeWrapper>
+      </Box>
+    </PageWrapper>
   );
 };
