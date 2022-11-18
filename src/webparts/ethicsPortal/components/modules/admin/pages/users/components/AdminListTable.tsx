5cf43aef4ca94352a177cafbe7fe8a17ea293d32
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2022-11-18 22:58:04 +0100

    table clean up

--- src/webparts/ethicsPortal/components/modules/admin/pages/users/components/AdminListTable.tsx
+++ src/webparts/ethicsPortal/components/modules/admin/pages/users/components/AdminListTable.tsx
@@ -1,31 +1,17 @@
-import { Box, Checkbox, IconButton } from "@material-ui/core";
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import { IconButton } from "@material-ui/core";
 import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
-import MaterialTable, { MTableToolbar } from "material-table";
+import MaterialTable from "material-table";
 import * as React from "react";
-import { CloseSharp, RemoveRedEye } from "@material-ui/icons";
-import { useToasts } from "react-toast-notifications";
-import { sp } from "@pnp/sp";
-import { errorAlert, successAlert } from "../../../../../utils/toast-messages";
-import { useHistory } from "react-router-dom";
-import { User } from "../forms/UserForm";
+import { CloseSharp } from "@material-ui/icons";
 import { UpdateAdminModal } from "../modals/UpdateAdminModal";
 import { Data } from "../ManageAdminPage";
 import { RemoveAdminModal } from "../modals/RemoveAdminModal";
 import { StaffData } from "./PeoplePicker";
+import {
+  TableHeaderStyles,
+  TableIcons,
+  TableStyles,
+} from "../../../../shared/components/TableCompHelpers";
 
 type Props = {
   users: StaffData[];
@@ -55,59 +41,7 @@ export const AdminListTable: React.FC<Props> = ({ users, loading }) => {
   return (
     <>
       <MaterialTable
-        icons={{
-          Add: React.forwardRef((props: any, ref: any) => (
-            <AddBox {...props} ref={ref} />
-          )),
-          Check: React.forwardRef((props: any, ref: any) => (
-            <Check {...props} ref={ref} />
-          )),
-          Clear: React.forwardRef((props: any, ref: any) => (
-            <Clear {...props} ref={ref} />
-          )),
-          Delete: React.forwardRef((props: any, ref: any) => (
-            <DeleteOutline {...props} ref={ref} />
-          )),
-          DetailPanel: React.forwardRef((props: any, ref: any) => (
-            <ChevronRight {...props} ref={ref} />
-          )),
-          Edit: React.forwardRef((props: any, ref: any) => (
-            <Edit {...props} ref={ref} />
-          )),
-          Export: React.forwardRef((props: any, ref: any) => (
-            <SaveAlt {...props} ref={ref} />
-          )),
-          Filter: React.forwardRef((props: any, ref: any) => (
-            <FilterList {...props} ref={ref} />
-          )),
-          FirstPage: React.forwardRef((props: any, ref: any) => (
-            <FirstPage {...props} ref={ref} />
-          )),
-          LastPage: React.forwardRef((props: any, ref: any) => (
-            <LastPage {...props} ref={ref} />
-          )),
-          NextPage: React.forwardRef((props: any, ref: any) => (
-            <ChevronRight {...props} ref={ref} />
-          )),
-          PreviousPage: React.forwardRef((props: any, ref: any) => (
-            <ChevronLeft {...props} ref={ref} />
-          )),
-          ResetSearch: React.forwardRef((props: any, ref: any) => (
-            <Clear {...props} ref={ref} />
-          )),
-          Search: React.forwardRef((props: any, ref: any) => (
-            <Search {...props} ref={ref} />
-          )),
-          SortArrow: React.forwardRef((props: any, ref: any) => (
-            <ArrowDownward {...props} ref={ref} />
-          )),
-          ThirdStateCheck: React.forwardRef((props: any, ref: any) => (
-            <Remove {...props} ref={ref} />
-          )),
-          ViewColumn: React.forwardRef((props: any, ref: any) => (
-            <ViewColumn {...props} ref={ref} />
-          )),
-        }}
+        icons={TableIcons}
         title={``}
         columns={columns}
         data={users}
@@ -123,19 +57,10 @@ export const AdminListTable: React.FC<Props> = ({ users, loading }) => {
           pageSizeOptions: [1, 2, 5],
           exportAllData: true,
           exportFileName: "Admins",
-          headerStyle: {
-            backgroundColor: "#FFCC00",
-            color: "black",
-            fontSize: "16px",
-          },
+          headerStyle: TableHeaderStyles,
           searchFieldVariant: "outlined",
         }}
-        style={{
-          boxShadow: "none",
-          width: "100%",
-          boxSizing: "border-box",
-          padding: "1rem",
-        }}
+        style={TableStyles}
         actions={[
           {
             icon: "visibility",
