79092b6c02d8267011792f298a859472bae7a6e8
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2022-11-18 23:03:40 +0100

    clean up

--- src/webparts/ethicsPortal/components/modules/employee/pages/ethics-policies/WriteUpPage.tsx
+++ src/webparts/ethicsPortal/components/modules/employee/pages/ethics-policies/WriteUpPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BlogSectionEnums } from "../../../admin/components/blog-set-up/sections/blog-section-enums/blog-section-enums";
 import { WriteUpLandingComponent } from "../../components/WriteUpLandingComponent";
 import { useParams } from "react-router-dom";
 
