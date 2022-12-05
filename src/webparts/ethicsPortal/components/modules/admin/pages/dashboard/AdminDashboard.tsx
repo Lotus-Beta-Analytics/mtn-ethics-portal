ebf209d5e8b308c24b04d6012c78a4d19f8593aa
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2022-12-05 22:11:47 +0100

    fix

--- src/webparts/ethicsPortal/components/modules/admin/pages/dashboard/AdminDashboard.tsx
+++ src/webparts/ethicsPortal/components/modules/admin/pages/dashboard/AdminDashboard.tsx
@@ -17,7 +17,7 @@ export const AdminDashboard = () => {
   const history = useHistory();
   return (
     <AdminWrapper>
-      <Box minHeight="100vh">
+      <Box height="100vh">
         <Box
           sx={{
             display: "grid",
@@ -46,64 +46,10 @@ export const AdminDashboard = () => {
   );
 };
 
-const dashboardItems = [
-  {
-    title: "Gallery",
-    link: "gallery",
-    icon: FaImages,
-  },
-
-  {
-    title: "Ethics Quiz",
-    link: "manage-quiz",
-    icon: FaQuestion,
-  },
-  {
-    title: "Recognition",
-    link: "recognition/manage",
-    icon: FaPeopleCarry,
-  },
-  {
-    title: "Training",
-    link: "training",
-    icon: FaPeopleCarry,
-  },
-  {
-    title: "Scrolling Text",
-    link: "scrolling-text",
-    icon: FaTextWidth,
-  },
-  {
-    title: "Ethics Policy Breaches",
-    link: "ethics/managedefaulters",
-    icon: FaPeopleCarry,
-  },
-  {
-    title: "Ethics Articles",
-    link: "manage-posts",
-    icon: FaBook,
-  },
-  {
-    title: "Configure Users",
-    link: "user/create",
-    icon: FaPeopleArrows,
-  },
-  {
-    title: "Ethics Policies",
-    link: "manage-policy",
-    icon: FaPeopleArrows,
-  },
-  {
-    title: "Carousel",
-    link: "carousel",
-    icon: FaPeopleArrows,
-  },
-];
-
 const DashboardCard = styled(Box)<BoxProps>({
   width: "auto",
   gap: 1.5,
-  height: "100px",
+  height: "120px",
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
