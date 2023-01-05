da369f98a0c323e3e7d65e3f099538db397c9cbb
FonsusAli <fonsusali@yahoo.com>
2023-01-05 14:44:01 +0100

    champion-activities

--- src/webparts/ethicsPortal/components/modules/employee/pages/recognition/champion-recognition/ethics-champion-activties/EthicsChampionsActivties.tsx
+++ src/webparts/ethicsPortal/components/modules/employee/pages/recognition/champion-recognition/ethics-champion-activties/EthicsChampionsActivties.tsx
@@ -1,18 +1,22 @@
+import { Box } from "@material-ui/core";
 import { sp } from "@pnp/sp";
 import * as React from "react";
+import { ImageContainerEthics } from "../../../../../../styles/styles";
+import { ContentType } from "../../../../../admin/pages/recognition/EthicsActivity";
 import { EmployeeWrapper } from "../../../../../shared/components/app-wrapper/employee/EmployeeWrapper";
 import { LandingPageHeaderWithImage } from "../../../../../shared/components/LandingPageHeaderWithImage";
+import { Label } from "../../../../components/Label";
 
 export const EthicsChampionsActivties = () => {
   const [items, setItems] = React.useState([]);
 
   React.useEffect(() => {
     sp.web.lists
-      .getByTitle(`EthicsActivities eq `)
+      .getByTitle(`EthicsActivities`)
       .items.get()
       .then((res) => {
-        setItems(res.slice(0, 3));
         setItems(res);
+        console.log(res);
       });
   }, []);
 
@@ -22,6 +26,9 @@ export const EthicsChampionsActivties = () => {
         bg="https://mtncloud.sharepoint.com/sites/MTNAppDevelopment/ethicsportal/assets/mtn-ethicslogo.png"
         text="Champion Recognition"
       />
+      <Box>
+        <div>Ethics Champion Activities</div>
+      </Box>
     </EmployeeWrapper>
   );
 };
